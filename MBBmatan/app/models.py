abb046efd79d545297cdc108eadf2444a99bb0bb
Andrey Valiev <andrei200854@gmail.com>
2025-02-28 20:07:43 +0300

    Database 1

--- MBBmatan/app/models.py
+++ MBBmatan/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Модель для хранения формул
+class Formulas(models.Model):
+    content = models.CharField(max_length=255)  # Сама формула
+    topic = models.CharField(max_length=100)    # Тема, к которой относится формула
+    description = models.TextField(blank=True, null=True)  # Описание формулы
+
+
+
+# Модель для хранения теории
+class Theory(models.Model):
+    content = models.TextField()  # Теоретический материал
+    topic = models.CharField(max_length=100)  # Тема, к которой относится теория
+
+
+
+# Модель для избранных формул пользователя
+class FavFormulas(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Связь с пользователем
+    formula = models.ForeignKey(Formulas, on_delete=models.CASCADE)  # Связь с формулой
+
+    class Meta:
+        unique_together = ('user', 'formula')  # Уникальность пары пользователь-формула
+
+    def __str__(self):
+        return f"{self.user.username} - {self.formula.topic}"
