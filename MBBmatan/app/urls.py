b1e858129c645cb5dc6de8a87a2dede1c5633eff
Artem <o.tarikov08@gmail.com>
2025-03-12 22:06:00 +0300

    обработанная версия меню входа в аккаунт(эльдар)

--- MBBmatan/app/urls.py
+++ MBBmatan/app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import RegisterView, CustomLoginView, ProfileView, home
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
-    path('accounts/login/', CustomLoginView.as_view(), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('', ProfileView.as_view(), name='profile'),
